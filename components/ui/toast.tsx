a9da8245204ae381f8b861f7a3b3014ef2051e64
Minhaj Shafin <minhajshafin@gmail.com>
2026-09-04 22:35:42 +0600

    fix: prevent error throw in useToast when context is not available

--- components/ui/toast.tsx
+++ components/ui/toast.tsx
@@ -74,7 +74,9 @@ export function ToastProvider({ children }: { children: React.ReactNode }) {
 export function useToast() {
   const context = useContext(ToastContext)
   if (!context) {
-    throw new Error('useToast must be used within a ToastProvider context.')
+    return {
+      toast: () => {},
+    }
   }
   return context
 }
